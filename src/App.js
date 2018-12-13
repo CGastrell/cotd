acb93c81e7e96c2db18db7c9e3d7fdd954d8cc3f
Christian Gastrell <cgastrell@gmail.com>
2018-12-13 18:12:33 -0300

    random creed on click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { default as creeds } from './creeds.json';
 import './App.css';
+import CreedCard from './components/CreedCard'
 
 class App extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      creed: 0,
+    }
+    this.creeds = creeds
+    this.randomCreed = this.randomCreed.bind( this );
+  }
+  randomCreed( event ) {
+    let newCreed = this.state.creed;
+    while ( newCreed === this.state.creed ) {
+      newCreed = Math.floor( Math.random() * this.creeds.length );
+    }
+    this.setState({
+      creed: newCreed
+    });
+  }
+
   render() {
+    let creedIndex = this.state.creed
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CreedCard onClick={ this.randomCreed } creed={ this.creeds[ creedIndex ] } />
       </div>
     );
   }
